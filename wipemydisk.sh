a7cdaacf2b45c6c67cb2e18545bf255a0c771fe1
AndyCrowd <AndyCrowd@users.noreply.github.com>
2014-12-07 15:59:55 +0100

    Update wipemydisk.sh
    added hexdump to verify and ability to use loop by defining the RepeatWipes variable.

--- wipemydisk.sh
+++ wipemydisk.sh
@@ -2,8 +2,12 @@
 
 #PathToDevice='/dev/sdX(Y)'
 PathToDevice="$1"
+RepeatWipes="0"
 
 if [ -b ${PathToDevice} ];then
+
+for (( count=0; count<=${RepeatWipes}; count++ ));do
+
 CC='[0-9]+$';
 DeviceName=$(echo ${PathToDevice} | sed 's/[0-9$]//m')
 PhysicalBlockSize=$(cat /sys/block/${DeviceName##*/}/queue/physical_block_size)
@@ -17,12 +21,16 @@ echo The ${PathToDevice} Is partition of the': ' ${DeviceName}
 echo PhysicalBlockSize = ${PhysicalBlockSize} , PartStart = ${PartStart} \
 , PartSectors = ${PartSectors} , PartInByteSize = ${PartInByteSize}
 
-#openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/random bs=128 count=1 2>/dev/null | \
-#base64)" -nosalt </dev/zero \
-#| pv -bartpes ${PartInByteSize} | dd bs=${PhysicalBlockSize} \ 
-#count=${PartSectors} of=/dev/${DeviceName##*/} seek=${PartStart} oflag=direct iflag=nocache 
+#openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/random bs=128 count=1 2>/dev/null | base64)" -nosalt </dev/zero \
+#| pv -bartpes ${PartInByteSize} |
+#dd bs=${PhysicalBlockSize} count=${PartSectors} of=/dev/${DeviceName##*/} seek=${PartStart} oflag=direct iflag=nocache 
 
-#dd if=/dev/zero bs=${PhysicalBlockSize} count=${PartSectors} |
+#dd if=/dev/urandom |
+#pv -bartpes ${PartInByteSize} |
+#dd of=/dev/${DeviceName##*/} bs=${PhysicalBlockSize} count=${PartSectors} \
+#seek=${PartStart} oflag=direct iflag=nocache
+
+#dd if=/dev/zero |
 #pv -bartpes ${PartInByteSize} | 
 #dd of=/dev/${DeviceName##*/} bs=${PhysicalBlockSize} count=${PartSectors} \ 
 #seek=${PartStart} oflag=direct iflag=nocache
@@ -41,5 +49,8 @@ echo PhysicalBlockSize = ${PhysicalBlockSize} , DeviceSectors = ${DeviceSectors}
 #dd of=/dev/${DeviceName##*/} seek=0 oflag=direct iflag=nocache 
 
 fi;
+done;
+#To verify 
+#hexdump "${PathToDevice}"
 else echo Is not a block'/'storage device'!'
 fi;
