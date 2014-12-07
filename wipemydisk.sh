65986b20cc66962deac54afa71bc9fe1f985bca2
AndyCrowd <AndyCrowd@users.noreply.github.com>
2014-12-07 18:12:14 +0100

    Update wipemydisk.sh
    change name of PhysicalBlockSize to UseBlockSize and redirected to use logical_block_size instead of physical_block_size because by checking with fdisk output and calculations it shows different size.

--- wipemydisk.sh
+++ wipemydisk.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #PathToDevice='/dev/sdX(Y)'
 PathToDevice="$1"
 RepeatWipes="0"
@@ -10,29 +9,30 @@ for (( count=0; count<=${RepeatWipes}; count++ ));do
 
 CC='[0-9]+$';
 DeviceName=$(echo ${PathToDevice} | sed 's/[0-9$]//m')
-PhysicalBlockSize=$(cat /sys/block/${DeviceName##*/}/queue/physical_block_size)
+#UseBlockSize=$(cat /sys/block/${DeviceName##*/}/queue/physical_block_size)
+UseBlockSize=$(cat /sys/block/${DeviceName##*/}/queue/logical_block_size)
 if [[ "${PathToDevice}" =~ $CC ]];then
 
 PartStart=$(cat /sys/block/${DeviceName##*/}/${PathToDevice##*/}/start)
 PartSectors=$(cat /sys/block/${DeviceName##*/}/${PathToDevice##*/}/size)
-PartInByteSize=$((PhysicalBlockSize * PartSectors))
+PartInByteSize=$((UseBlockSize * PartSectors))
 
 echo The ${PathToDevice} Is partition of the': ' ${DeviceName} 
-echo PhysicalBlockSize = ${PhysicalBlockSize} , PartStart = ${PartStart} \
+echo UseBlockSize = ${UseBlockSize} , PartStart = ${PartStart} \
 , PartSectors = ${PartSectors} , PartInByteSize = ${PartInByteSize}
 
 #openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/random bs=128 count=1 2>/dev/null | base64)" -nosalt </dev/zero \
 #| pv -bartpes ${PartInByteSize} |
-#dd bs=${PhysicalBlockSize} count=${PartSectors} of=/dev/${DeviceName##*/} seek=${PartStart} oflag=direct iflag=nocache 
+#dd bs=${UseBlockSize} count=${PartSectors} of=/dev/${DeviceName##*/} seek=${PartStart} oflag=direct iflag=nocache 
 
 #dd if=/dev/urandom |
 #pv -bartpes ${PartInByteSize} |
-#dd of=/dev/${DeviceName##*/} bs=${PhysicalBlockSize} count=${PartSectors} \
+#dd of=/dev/${DeviceName##*/} bs=${UseBlockSize} count=${PartSectors} \
 #seek=${PartStart} oflag=direct iflag=nocache
 
 #dd if=/dev/zero |
 #pv -bartpes ${PartInByteSize} | 
-#dd of=/dev/${DeviceName##*/} bs=${PhysicalBlockSize} count=${PartSectors} \ 
+#dd of=/dev/${DeviceName##*/} bs=${UseBlockSize} count=${PartSectors} \ 
 #seek=${PartStart} oflag=direct iflag=nocache
 
 else echo The ${PathToDevice} is a device'!';
@@ -40,12 +40,12 @@ else echo The ${PathToDevice} is a device'!';
 partprobe ${PathToDevice}
 
 DeviceSectors=$(cat /sys/block/${DeviceName##*/}/size)
-DeviceInByteSize=$((PhysicalBlockSize * DeviceSectors))
-echo PhysicalBlockSize = ${PhysicalBlockSize} , DeviceSectors = ${DeviceSectors} \
+DeviceInByteSize=$((UseBlockSize * DeviceSectors))
+echo UseBlockSize = ${UseBlockSize} , DeviceSectors = ${DeviceSectors} \
 , DeviceInByteSize = ${DeviceInByteSize}
 
 #wipefs -a ${PathToDevice}
-#dd if=/dev/zero bs=${PhysicalBlockSize} count=${DeviceSectors} | pv -bartpes ${DeviceInByteSize} | 
+#dd if=/dev/zero bs=${UseBlockSize} count=${DeviceSectors} | pv -bartpes ${DeviceInByteSize} | 
 #dd of=/dev/${DeviceName##*/} seek=0 oflag=direct iflag=nocache 
 
 fi;
