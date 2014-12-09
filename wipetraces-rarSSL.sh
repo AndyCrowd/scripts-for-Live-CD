30eb464d0d782b7a3616d4a71113d607dbd9d2af
AndyCrowd <AndyCrowd@users.noreply.github.com>
2014-12-09 17:35:54 +0100

    Update wipetraces-rarSSL.sh
    split line

--- wipetraces-rarSSL.sh
+++ wipetraces-rarSSL.sh
@@ -5,4 +5,5 @@
 # It removes files when carshes due disk full error and removes all created files
 #
 openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/random bs=128 \
-count=1 2>/dev/null | base64)" -nosalt </dev/zero | pv | rar a -sistdin -p${RANDOM}"$(dd if=/dev/urandom bs=4086 count=1)" -v${RANDOM}m test.wipe.${RANDOM}.file
+count=1 2>/dev/null | base64)" -nosalt </dev/zero | pv | 
+rar a -sistdin -p${RANDOM}"$(dd if=/dev/urandom bs=4086 count=1)" -v${RANDOM}m test.wipe.${RANDOM}.file
