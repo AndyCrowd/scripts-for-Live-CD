aa81c4bb715649fc10f5763f6c5881e5c5df579c
AndyCrowd <AndyCrowd@users.noreply.github.com>
2014-12-09 06:57:43 +0100

    Create wipetraces-rarSSL.sh
    wipes traces by creating files until partition ends

---
+++ wipetraces-rarSSL.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# Creates rar with random password, file names, size, split name
+# It removes files when carshes due disk full error and removes all created files
+#
+openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/random bs=128 \
+count=1 2>/dev/null | base64)" -nosalt </dev/zero | pv | rar a -sistdin -p${RANDOM}"$(dd if=/dev/urandom bs=4086 count=1)" -v${RANDOM}m test.wipe.${RANDOM}.file
